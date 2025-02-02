9ad5eac9ce1267ee486374d2746145797bb8ffd6
Matthieu Metz <metz.matthieu@gmail.com>
2025-02-02 22:26:02 +0100

    add the capitalize first letter for the Month in GetMonth

--- src/containers/ModalEvent/index.js
+++ src/containers/ModalEvent/index.js
@@ -3,41 +3,56 @@ import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
-const ModalEvent = ({ event }) => (
-  <div className="ModalEvent">
-    <div className="ModalEvent__imageContainer">
-      <img
-        data-testid="card-image-testid"
-        src={event.cover}
-        alt={event.title}
-      />
-    </div>
-    <div className="ModalEvent__title">
-      <div className="ModalEvent__titleLabel">{event.title}</div>
-      <div className="ModalEvent__titlePeriode">
-        {`${event.periode} ${getMonth(new Date(event.date))}`}
+// Fonction utilitaire locale pour capitaliser la première lettre
+const capitalizeFirstLetter = (string) =>
+  string.charAt(0).toUpperCase() + string.slice(1);
+
+const ModalEvent = ({ event }) => {
+  const month = capitalizeFirstLetter(getMonth(new Date(event.date)));
+
+  return (
+    <div className="ModalEvent">
+      <div className="ModalEvent__imageContainer">
+        <img
+          data-testid="card-image-testid"
+          src={event.cover}
+          alt={event.title}
+        />
+      </div>
+      <div className="ModalEvent__title">
+        <div className="ModalEvent__titleLabel">{event.title}</div>
+        <div className="ModalEvent__titlePeriode">
+          {`${event.periode} ${month}`}
+        </div>
+      </div>
+      <div className="ModalEvent__descriptionContainer">
+        <h3>Description</h3>
+        <div>{event.description}</div>
+      </div>
+      <div className="ModalEvent__descriptionContainer">
+        <h3>Participants</h3>
+        <div>{event.nb_guesses} participants</div>
+      </div>
+      <div className="ModalEvent__descriptionContainer">
+        <h3>Prestations</h3>
+        {event.prestations.map((presta) => (
+          <div key={presta}>{presta}</div>
+        ))}
       </div>
     </div>
-    <div className="ModalEvent__descriptionContainer">
-      <h3>Description</h3>
-      <div>{event.description}</div>
-    </div>
-    <div className="ModalEvent__descriptionContainer">
-      <h3>Participants</h3>
-      <div>{event.nb_guesses} participants</div>
-    </div>
-    <div className="ModalEvent__descriptionContainer">
-      <h3>Prestations</h3>
-      {event.prestations.map((presta) => (
-        <div key={presta}>{presta}</div>
-      ))}
-    </div>
-  </div>
-);
+  );
+};
 
 ModalEvent.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  event: PropTypes.any.isRequired,
+  event: PropTypes.shape({
+    cover: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    periode: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    nb_guesses: PropTypes.number.isRequired,
+    prestations: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
 };
 
 export default ModalEvent;
